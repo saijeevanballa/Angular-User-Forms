9e4500c71bf08f23d353fbe57c666500c2bb874c
saijeevanballa <saijeevanballa@gmail.com>
2020-04-21 18:20:56 +0530

    1 file added

---
+++ src/app/services/store.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class StoreService {
+  constructor() {}
+  create(key: string, value: any) {
+    try {
+      localStorage.setItem(key, JSON.stringify(value));
+    } catch (err) {
+      console.error("Error saving to localStorage", err);
+    }
+  }
+
+  find(key: string) {
+    try {
+      return JSON.parse(localStorage.getItem(key));
+    } catch (err) {
+      console.error("Error getting data from localStorage", err);
+      return null;
+    }
+  }
+
+  remove(key: string) {
+    try {
+      localStorage.removeItem(key);
+      return true;
+    } catch (err) {
+      console.error("Error Deleteing data from localStorage", err);
+      return null;
+    }
+  }
+  
+}
